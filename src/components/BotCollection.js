9eb54f64006da91d6cc33c16786eaa28079ee14b
John-thabuks <thabuksjohn@gmail.com>
2023-12-04 00:25:49 +0300

    See profiles of all bots rendered in BotCollection.

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import BotCard from "./BotCard"
 
 function BotCollection() {
   // Your code here
+
+  const [bots, setBots] = useState([])
+
+  useEffect(() =>{
+    fetch("http://localhost:8002/bots")
+      .then((response) => {
+        if(!response.ok){
+          throw new Error("Couldn't fetch the bot data")
+        }    
+        return response.json()    
+      })
+      .then((data) => setBots(data))
+      .catch((error) => console.error("Error encountered", error))
+  }, [])
+
   return (
     <div className="ui four column grid">
       <div className="row">
         {/*...and here..*/}
-        Collection of all bots
+        {bots.map((bot) => (
+          <BotCard key={bot.id} bot= {bot}/>
+        ))}
       </div>
     </div>
   );
