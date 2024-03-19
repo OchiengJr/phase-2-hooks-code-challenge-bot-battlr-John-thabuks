6d6ec8f64cffb790bafc67443479345261a1c323
OchiengJr <chrispine.ochieng@student.moringaschool.com>
2024-03-19 10:07:04 +0300

    Phase 2 Code Challenge: Bot Battlr

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import BotsPage from "./BotsPage";
 
+// The root component of the application
 function App() {
+  // Render the BotsPage component inside a containing div
   return (
     <div className="App">
       <BotsPage />
